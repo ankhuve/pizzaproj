51b78afbe6d4ba2af1cfe7fd9768fe8cdfd923a6
mfrisell <mfrisell@kth.se>
2016-03-12 12:38:05 +0100

    hemisphere light, commented away

--- app/scripts/1setUpTerrain.js
+++ app/scripts/1setUpTerrain.js
@@ -28,6 +28,13 @@ renderer.shadowMapSoft = false;
     // add a light behind
     var light	= new THREE.DirectionalLight('white', 0.75*2);
     light.position.set(-0.5, -0.5, -2);
+
+
+    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+    hemiLight.color.setHSL( 0.6, 1, 0.6 );
+    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    hemiLight.position.set( 0, 500, 0 );
+    //scene.add( hemiLight );
     //scene.add( light )
 })();
 
