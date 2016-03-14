1b3d19374bf73dcfdba2df82fca6be946051a494
mfrisell <mfrisell@kth.se>
2016-03-14 14:27:04 +0100

    search genre too

--- app/scripts/6movieSearch.js
+++ app/scripts/6movieSearch.js
@@ -11,9 +11,10 @@ function searchMovies(term) {
 		for(var i=0; i<arrayOfTreePos.length;i++) {
 			thisMovie = arrayOfTreePos[i];
 			thisMovieTitle = thisMovie.data[0].toLowerCase();
+			thisMovieGenre = thisMovie.data[7].toLowerCase();
 			thisMovieActors = thisMovie.data[10].toLowerCase();
 
-			if((thisMovieTitle.indexOf(term.toLowerCase())!=-1) || (thisMovieActors.indexOf(term.toLowerCase())!=-1)) {
+			if((thisMovieTitle.indexOf(term.toLowerCase())!=-1) || (thisMovieActors.indexOf(term.toLowerCase())!=-1) || (thisMovieGenre.indexOf(term.toLowerCase())!=-1)) {
 				for(var k=0; k<scene.children[3].children.length;k++) {
 					if(scene.children[3].children[k].position.x == thisMovie.x && scene.children[3].children[k].position.z == thisMovie.z){
 						scene.children[3].children[k].visible = true;
