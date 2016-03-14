50a66bb4ba3da9ba97f87048c157c68e4be6299f
jonsod93 <jonsod93@gmail.com>
2016-03-14 15:45:27 +0100

    Toggle and icons

--- app/scripts/4initiateCameraAndKeybindings.js
+++ app/scripts/4initiateCameraAndKeybindings.js
@@ -1,38 +1,37 @@
-
 var camera, scene;
 var geometry, material, mesh;
 var controls;
-var mapCamera, mapWidth = window.innerWidth, mapHeight = window.innerHeight + 220;
+var mapCamera, mapWidth = window.innerWidth,
+    mapHeight = window.innerHeight + 220;
 
 var objects = [];
 
 var raycaster;
 
-var blocker = document.getElementById( 'blocker' );
-var instructions = document.getElementById( 'instructions' );
+var blocker = document.getElementById('blocker');
+var instructions = document.getElementById('instructions');
 
 //  marker that follows player
 var material = new THREE.MeshBasicMaterial({
-	color: 0xffffff
+    color: 0xffffff
 });
 var radius = 2;
 var segments = 32;
-var circleGeometry = new THREE.CircleGeometry( radius, segments );
-var circle = new THREE.Mesh( circleGeometry, material );
-circle.rotation.x = -Math.PI/2;
+var circleGeometry = new THREE.CircleGeometry(radius, segments);
+var circle = new THREE.Mesh(circleGeometry, material);
+circle.rotation.x = -Math.PI / 2;
 //scene.add( circle );
-sceneMiniMap.add( circle ); 
-  
+sceneMiniMap.add(circle);
+
 // orthographic cameras (minimap)
-mapCamera = new THREE.OrthographicCamera(
- -62,		// Left  // 
-window.innerWidth / 2,		// Right 
-window.innerHeight / 2,		// Top 
--200,	// Bottom 
--5000,            			// Near 
-10000 );           			// Far 
-mapCamera.up = new THREE.Vector3(0,0,-1);  //rotation i x,y,z på mappen
-mapCamera.lookAt( new THREE.Vector3(0,-1,0) );
+mapCamera = new THREE.OrthographicCamera(-62, // Left  // 
+    window.innerWidth / 2, // Right 
+    window.innerHeight / 2, // Top 
+    -200, // Bottom 
+    -5000, // Near 
+    10000); // Far 
+mapCamera.up = new THREE.Vector3(0, 0, -1); //rotation i x,y,z på mappen
+mapCamera.lookAt(new THREE.Vector3(0, -1, 0));
 mapCamera.position.y = 500;
 
 scene.add(mapCamera);
@@ -42,93 +41,93 @@ THREEx.WindowResize(renderer, mapCamera);
 
 var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
 
-if ( havePointerLock ) {
+if (havePointerLock) {
 
-	var element = document.body;
+    var element = document.body;
 
-	var pointerlockchange = function ( event ) {
+    var pointerlockchange = function (event) {
 
-		if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+        if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
 
-			controlsEnabled = true;
-			controls.enabled = true;
+            controlsEnabled = true;
+            controls.enabled = true;
 
-			blocker.style.display = 'none';
-			$("#centerSign").css("display", "block");
-			audio.play();
+            blocker.style.display = 'none';
+            $("#centerSign").css("display", "block");
+            audio.play();
 
-		} else {
-			//controlsEnabled = false;
-			controls.enabled = false;
+        } else {
+            //controlsEnabled = false;
+            controls.enabled = false;
 
-			blocker.style.display = '-webkit-box';
-			blocker.style.display = '-moz-box';
-			blocker.style.display = 'box';
+            blocker.style.display = '-webkit-box';
+            blocker.style.display = '-moz-box';
+            blocker.style.display = 'box';
 
-			instructions.style.display = '';
-			$("#centerSign").css("display", "none");
-			audio.play();
-			audio.pause();
-			
+            instructions.style.display = '';
+            $("#centerSign").css("display", "none");
+            audio.play();
+            audio.pause();
 
-		}
 
-	};
+        }
 
-	var pointerlockerror = function ( event ) {
+    };
 
-		instructions.style.display = '';
+    var pointerlockerror = function (event) {
 
-	};
+        instructions.style.display = '';
 
-	// Hook pointer lock state change events
-	document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-	document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-	document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+    };
 
-	document.addEventListener( 'pointerlockerror', pointerlockerror, false );
-	document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
-	document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+    // Hook pointer lock state change events
+    document.addEventListener('pointerlockchange', pointerlockchange, false);
+    document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+    document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
 
-	instructions.addEventListener( 'click', function ( event ) {
+    document.addEventListener('pointerlockerror', pointerlockerror, false);
+    document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+    document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
 
-		instructions.style.display = 'none';
+    instructions.addEventListener('click', function (event) {
 
-		// Ask the browser to lock the pointer
-		element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+        instructions.style.display = 'none';
 
-		if ( /Firefox/i.test( navigator.userAgent ) ) {
+        // Ask the browser to lock the pointer
+        element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
 
-			var fullscreenchange = function ( event ) {
+        if (/Firefox/i.test(navigator.userAgent)) {
 
-				if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+            var fullscreenchange = function (event) {
 
-					document.removeEventListener( 'fullscreenchange', fullscreenchange );
-					document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+                if (document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element) {
 
-					element.requestPointerLock();
-				}
+                    document.removeEventListener('fullscreenchange', fullscreenchange);
+                    document.removeEventListener('mozfullscreenchange', fullscreenchange);
 
-			};
+                    element.requestPointerLock();
+                }
 
-			document.addEventListener( 'fullscreenchange', fullscreenchange, false );
-			document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+            };
 
-			element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+            document.addEventListener('fullscreenchange', fullscreenchange, false);
+            document.addEventListener('mozfullscreenchange', fullscreenchange, false);
 
-			element.requestFullscreen();
+            element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
 
-		} else {
+            element.requestFullscreen();
 
-			element.requestPointerLock();
+        } else {
 
-		}
+            element.requestPointerLock();
 
-	}, false );
+        }
+
+    }, false);
 
 } else {
 
-	instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+    instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
 
 }
 
@@ -151,121 +150,124 @@ var isMoving = 0;
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 
-	camera.position.y = 0;
-	camera.position.z = 0;
+    camera.position.y = 0;
+    camera.position.z = 0;
 
-	controls = new THREE.PointerLockControls( camera );
-	controls.getObject().rotation.y = -Math.PI/2;
-	scene.add( controls.getObject() );
+    controls = new THREE.PointerLockControls(camera);
+    controls.getObject().rotation.y = -Math.PI / 2;
+    scene.add(controls.getObject());
 
-	footsteps = new Audio('sounds/steps.mp3');
-	footsteps.loop = true;
-	footsteps.volume = 0.1;
-	footsteps.playbackRate = 1.5;
+    footsteps = new Audio('sounds/steps.mp3');
+    footsteps.loop = true;
+    footsteps.volume = 0.1;
+    footsteps.playbackRate = 1.5;
 
-	var onKeyDown = function ( event ) {
+    var onKeyDown = function (event) {
 
-		switch ( event.keyCode ) {
+        switch (event.keyCode) {
 
-			case 38: // up
-			case 87: // w
-				if(controls.enabled){
-					moveForward = true;
-					footsteps.play();
-				}
-				break;
+        case 38: // up
+        case 87: // w
+            if (controls.enabled) {
+                moveForward = true;
+                footsteps.play();
+            }
+            break;
 
-			case 37: // left
-			case 65: // a
-				if(controls.enabled){
-					moveLeft = true;
-					footsteps.play();
-				}
-				break;
+        case 37: // left
+        case 65: // a
+            if (controls.enabled) {
+                moveLeft = true;
+                footsteps.play();
+            }
+            break;
 
-			case 40: // down
-			case 83: // s
-				if(controls.enabled){
-					moveBackward = true;
-					footsteps.play();
-				}
-				break;
+        case 40: // down
+        case 83: // s
+            if (controls.enabled) {
+                moveBackward = true;
+                footsteps.play();
+            }
+            break;
 
-			case 39: // right
-			case 68: // d
-				if(controls.enabled){
-					moveRight = true;
-					footsteps.play();
-				}
-				break;
+        case 39: // right
+        case 68: // d
+            if (controls.enabled) {
+                moveRight = true;
+                footsteps.play();
+            }
+            break;
 
-			//case 32: // kanske lägga till flygfunktion?! trycker man space så börjar man flyga eller nåt
-			//	if ( canJump === true ) velocity.y += 350;
-			//	canJump = false;
-			//	break;
+        case 73:
+            if (controls.enabled && closeTrees.length > 0) {
+                togglePlot();
+            }
+            break;
 
-		}
-		// moved this outside onKeyDown event to load camera instantly
-		// renderer.setSize( window.innerWidth, window.innerHeight );
-		// renderer.setClearColor( 0x000000, 1 );
-		// renderer.autoClear = false;
+            //case 32: // kanske lägga till flygfunktion?! trycker man space så börjar man flyga eller nåt
+            //	if ( canJump === true ) velocity.y += 350;
+            //	canJump = false;
+            //	break;
 
-	};
+        }
+        // moved this outside onKeyDown event to load camera instantly
+        // renderer.setSize( window.innerWidth, window.innerHeight );
+        // renderer.setClearColor( 0x000000, 1 );
+        // renderer.autoClear = false;
 
-	// I put this outside the onKeyDown event to load camera instantly
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setClearColor( 0x000000, 1 );
-	renderer.autoClear = false;
+    };
 
-	var onKeyUp = function ( event ) {
+    // I put this outside the onKeyDown event to load camera instantly
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000, 1);
+    renderer.autoClear = false;
 
-		switch( event.keyCode ) {
+    var onKeyUp = function (event) {
 
-			case 38: // up
-			case 87: // w
-				moveForward = false;
-				footsteps.pause();
-				break;
+        switch (event.keyCode) {
 
-			case 37: // left
-			case 65: // a
-				moveLeft = false;
-				footsteps.pause();
-				break;
+        case 38: // up
+        case 87: // w
+            moveForward = false;
+            footsteps.pause();
+            break;
 
-			case 40: // down
-			case 83: // s
-				moveBackward = false;
-				footsteps.pause();
-				break;
+        case 37: // left
+        case 65: // a
+            moveLeft = false;
+            footsteps.pause();
+            break;
 
-			case 39: // right
-			case 68: // d
-				moveRight = false;
-				footsteps.pause();
-				break;
+        case 40: // down
+        case 83: // s
+            moveBackward = false;
+            footsteps.pause();
+            break;
 
-		}
+        case 39: // right
+        case 68: // d
+            moveRight = false;
+            footsteps.pause();
+            break;
 
-	};
+        }
 
-	document.addEventListener( 'keydown', onKeyDown, false );
-	document.addEventListener( 'keyup', onKeyUp, false );
-	window.addEventListener( 'resize', onWindowResize, false );
+    };
+
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+    window.addEventListener('resize', onWindowResize, false);
 
 
 }
 
 function onWindowResize() {
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
-
-
-
