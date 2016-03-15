d68208fb68e626c8647476e13b5ab05f580bbc73
jonsod93 <jonsod93@gmail.com>
2016-03-15 13:23:57 +0100

    moved minimap

--- app/scripts/5render.js
+++ app/scripts/5render.js
@@ -1,46 +1,51 @@
-var prevPos = {"x":0, "y":0, "z":0};
+var prevPos = {
+    "x": 0,
+    "y": 0,
+    "z": 0
+};
 var moviePrev = "";
 
 
 function animate() {
-	var w = window.innerWidth, h = window.innerHeight;
+    var w = window.innerWidth,
+        h = window.innerHeight;
 
-	if(!controls.enabled) {
-		controls.getObject().rotation.y -= Math.PI/5000;
-	}
+    if (!controls.enabled) {
+        controls.getObject().rotation.y -= Math.PI / 5000;
+    }
 
-	//mapCamera.rotation.z = controls.getObject().rotation.y;
+    //mapCamera.rotation.z = controls.getObject().rotation.y;
 
-	//cloudObj.position.x +=Math.PI/50;
-	//circle.position.x = controls.getObject().position.x;
-	triangle.position.set( controls.getObject().position.x, controls.getObject().position.y+10, controls.getObject().position.z )
-	//triangle.rotation.x = -Math.PI/2;
-	triangle.rotation.x = -Math.PI/2;
-	triangle.rotation.z = (controls.getObject().rotation.y)-44.91;
+    //cloudObj.position.x +=Math.PI/50;
+    //circle.position.x = controls.getObject().position.x;
+    triangle.position.set(controls.getObject().position.x, controls.getObject().position.y + 10, controls.getObject().position.z)
+        //triangle.rotation.x = -Math.PI/2;
+    triangle.rotation.x = -Math.PI / 2;
+    triangle.rotation.z = (controls.getObject().rotation.y) - 44.91;
 
 
-	//console.log(circle.position.x);
+    //console.log(circle.position.x);
 
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
     movementAndDetailsOnDemand();
 
-	// setViewport parameters:
-	renderer.setViewport( 0, 0, w, h );
-	renderer.clear();
+    // setViewport parameters:
+    renderer.setViewport(0, 0, w, h);
+    renderer.clear();
 
-	// Depth of Field
-	//depthOfField= new THREEx.DepthOfField(renderer)
-	//depthOfField.render(scene, camera)
-	
-	// full display
-	
-	renderer.render( scene, camera );
+    // Depth of Field
+    //depthOfField= new THREEx.DepthOfField(renderer)
+    //depthOfField.render(scene, camera)
 
-	// minimap (overhead orthogonal camera)
-	//  lower_left_x, lower_left_y, viewport_width, viewport_height
-	renderer.setViewport(0, 200, mapWidth, mapHeight );
-	renderer.render( sceneMiniMap, mapCamera);
+    // full display
+
+    renderer.render(scene, camera);
+
+    // minimap (overhead orthogonal camera)
+    //  lower_left_x, lower_left_y, viewport_width, viewport_height
+    renderer.setViewport(0, h - mapHeight, mapWidth, mapHeight);
+    renderer.render(sceneMiniMap, mapCamera);
 
 
 }
